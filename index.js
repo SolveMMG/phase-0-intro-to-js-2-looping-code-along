ebae48d4ee093ab321228fccd19e1ddcaebac944
Your Name <mikachupichu@gmail.com>
2023-09-15 12:12:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+  names=['Michael','Igni','Francisca']
+  const eventName= 'surprise'
+  function writeCards(names, eventName) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    messages.push(message);
+    }
+    return messages;
+  }
+
+function countDown(pinteger){
+    while(pinteger>=0){
+    console.log(pinteger);
+    pinteger--;
+  }
+}
+countDown(10);
